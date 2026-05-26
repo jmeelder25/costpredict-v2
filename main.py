ea06fa69cbfd28725b40dea82eb36af72aa2289b
jmeelder25 <ceo@costpredict.com>
2026-05-25 20:47:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -84,4 +84,5 @@ def generate_pdf():
     return response
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    # Render provides the port in an environment variable
+    app.run(host='0.0.0.0', port=port, debug=False)
